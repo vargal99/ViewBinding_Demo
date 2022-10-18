23c3ed6e8e346b80eaaaf2f3252fc9134173645f
Lacika <vargalacii111@gmail.com>
2022-10-18 15:46:04 +0200

    ViewBinding

---
+++ app/src/main/java/com/example/android/viewbindingdemo/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.android.viewbindingdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.android.viewbindingdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.apply {
+            btnSubmit.setOnClickListener {
+                val name = etName.text.toString()
+                tvMessage.text = "Hello $name"
+                etName.setText("")
+            }
+        }
+    }
+}
